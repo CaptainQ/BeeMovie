0d7f816c7d9538a9167ee22aef6153056e480998
Patrick McCaffrey <allknowingq@gmail.com>
2018-03-24 18:14:07 -0400

    Update beebot.js

--- beebot.js
+++ beebot.js
@@ -11,4 +11,10 @@ client.on("message", (message) => {
     }
 });
 
+client.on("message", (message) => {
+    if (message.author.avatar == "3523") {
+        message.reply("test");
+    }
+});
+
 client.login(process.env.BOT_TOKEN);
